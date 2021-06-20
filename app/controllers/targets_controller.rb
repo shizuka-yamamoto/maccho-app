5ffcf854034a72c501a6413395d0a253409eb850
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-20 17:46:44 +0900

    新規投稿機能の実装

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,7 +1,24 @@
 class TargetsController < ApplicationController
   def index
+    @targets = Target.includes(:user)
   end
 
   def new
+    @target = Target.new
   end
+
+  def create
+    @target = Target.new(target_params)
+    if @target.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def target_params
+    params.require(:target).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
