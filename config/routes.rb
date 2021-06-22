fbdd6417dcce3012fed6a6c7edc7a51d28243d66
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-22 22:05:08 +0900

    投稿詳細ページの準備

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "targets#index"
   resources :users, only: [:edit, :update]
-  resources :targets, only: [:new, :create]
+  resources :targets, only: [:new, :create, :show]
 end
