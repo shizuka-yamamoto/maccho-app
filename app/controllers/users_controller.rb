45012f64a631288835b0bf3d167b63d1ab0320ab
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-29 19:39:18 +0900

    ログインの有無による表示の切り替え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :authenticate_user!, except: :show
+  before_action :authenticate_user!, except: :index
 
   def edit
   end
