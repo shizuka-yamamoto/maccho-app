bb7d954e8525eb254698006516f2759e11a530a1
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-07-10 14:44:45 +0900

    reviewモデルの単体テストコードの実装

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :review do
+    text { Faker::Lorem.sentence }
+    achievement_id { 2 }
+    association :user
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @review = FactoryBot.build(:review)
+  end
+
+  describe '投稿の保存' do
+    context '投稿が保存できる場合' do
+      it 'テキストがありachievement_id1以外であれば投稿できる' do
+        expect(@review).to be_valid
+      end
+    end
+    it 'achievement_idがid:1以外であれば登録できる' do
+      @review.achievement_id = 2
+      expect(@review).to be_valid
+    end
+
+    context '投稿が保存できない場合' do
+      it 'テキストが空では投稿できない' do
+        @review.text = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Textを入力してください")
+      end
+      it 'achievement_idがid:1では登録できない' do
+        @review.achievement_id = 1
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Achievementは1以外の値にしてください")
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @review.user = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include('Userを入力してください')
+      end
+    end
+  end
 end
