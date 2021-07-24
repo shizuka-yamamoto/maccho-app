3f6629478c240b8c71c6b3f0329c5f7ed7bdd4e9
shizuka <shiiishim920@gmail.com>
2021-07-24 11:12:26 +0900

    コメントコントローラーのパスの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_prarms)
-    redirect_to "/targets/#{comment.target.id}"
+    comment = current_user.comments.create(comment_params)
+    redirect_to target_path(comment.target)
   end
 
   private
 
-  def comment_prarms
+  def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, target_id: params[:target_id])
   end
 end
